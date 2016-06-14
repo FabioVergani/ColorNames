9d601f463b5bf90722ebeced4ac3a13fb4c52d77
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-06-14 18:15:44 +0200

    Create SupportedbyAllBrowsers.js

---
+++ SupportedbyAllBrowsers.js
@@ -0,0 +1,156 @@
+ColorNameSupportedbyAllBrowsers={
+white:								[255,255,255],//basic16
+ivory:								[255,255,240],
+lightyellow:					[255,255,224],
+yellow:							[255,255,0],//basic15
+snow:								[255,250,250],
+floralwhite:					[255,250,240],
+lemonchiffon:				[255,250,205],
+cornsilk:						[255,248,220],
+seashell:						[255,245,238],
+lavenderblush:				[255,240,245],
+papayawhip:					[255,239,213],
+blanchedalmond:			[255,235,205],
+mistyrose:						[255,228,225],
+bisque:							[255,228,196],
+moccasin:						[255,228,181],
+navajowhite:				[255,222,173],
+peachpuff:						[255,218,185],
+gold:								[255,215,0],
+pink:								[255,192,203],
+lightpink:						[255,182,193],
+orange:							[255,165,0],
+lightsalmon:					[255,160,122],
+darkorange:					[255,140,0],
+coral:								[255,127,80],
+hotpink:							[255,105,180],
+tomato:							[255,99,71],
+orangered:					[255,69,0],
+deeppink:						[255,20,147],
+fuchsia:							[255,0,255],//basic14 , alias: magenta:
+red:									[255,0,0],//basic13
+oldlace:							[253,245,230],
+lightgoldenrodyellow:[250,250,210],
+linen:								[250,240,230],
+antiquewhite:				[250,235,215],
+salmon:							[250,128,114],
+ghostwhite:					[248,248,255],
+mintcream:					[245,255,250],
+whitesmoke:					[245,245,245],
+beige:								[245,245,220],
+wheat:							[245,222,179],
+sandybrown:				[244,164,96],
+azure:								[240,255,255],
+honeydew:					[240,255,240],
+aliceblue:						[240,248,255],
+khaki:								[240,230,140],
+lightcoral:						[240,128,128],
+palegoldenrod:			[238,232,170],
+violet:							[238,130,238],
+crimson:						[237,20,61],
+darksalmon:					[233,150,122],
+lavender:						[230,230,250],
+lightcyan:						[224,255,255],
+burlywood:					[222,184,135],
+plum:								[221,160,221],
+gainsboro:					[220,220,220],
+palevioletred:				[219,112,147],
+goldenrod:					[218,165,32],
+orchid:							[218,112,214],
+thistle:							[216,191,216],
+lightgray:						[211,211,211],//alias: lightgrey
+tan:									[210,180,140],
+chocolate:						[210,105,30],
+peru:								[205,133,63],
+indianred:						[205,92,92],
+mediumvioletred:		[199,21,133],
+silver:								[192,192,192],//basic12
+darkkhaki:						[189,183,107],
+rosybrown:					[188,143,143],
+mediumorchid:			[186,85,211],
+darkgoldenrod:			[184,134,11],
+firebrick:						[178,34,34],
+powderblue:					[176,224,230],
+lightsteelblue:				[176,196,222],
+paleturquoise:				[175,238,238],
+greenyellow:				[173,255,47],
+lightblue:						[173,216,230],
+darkgray:						[169,169,169],//alias: darkgrey
+brown:							[165,42,42],
+sienna:							[160,82,45],
+yellowgreen:				[154,205,50],
+darkorchid:					[153,50,204],
+palegreen:						[152,251,152],
+darkviolet:					[148,0,211],
+mediumpurple:			[147,112,219],
+lightgreen:					[144,238,144],
+darkseagreen:				[143,188,143],
+saddlebrown:				[139,69,19],
+darkmagenta:				[139,0,139],
+darkred:							[139,0,0],
+blueviolet:						[138,43,226],
+lightskyblue:				[135,206,250],
+skyblue:							[135,206,235],
+gray:								[128,128,128],//basic11, alias: grey
+olive:								[128,128,0],//basic10
+purple:							[128,0,128],//basic9
+maroon:							[128,0,0],//basic8
+aquamarine:					[127,255,212],
+chartreuse:					[127,255,0],
+lawngreen:					[124,252,0],
+mediumslateblue:		[123,104,238],
+lightslategray:				[119,136,153],//alias: lightslategrey
+slategray:						[112,128,144],//alias: slategrey
+olivedrab:						[107,142,35],
+slateblue:						[106,90,205],
+mediumaquamarine:	[102,205,170],
+rebeccapurple:				[102,51,153],
+cornflowerblue:			[100,149,237],
+cadetblue:						[95,158,160],
+darkolivegreen:			[85,107,47],
+indigo:							[75,0,130],
+mediumturquoise:		[72,209,204],
+darkslateblue:				[72,61,139],
+steelblue:						[70,130,180],
+royalblue:						[65,105,225],
+turquoise:						[64,224,208],
+mediumseagreen:		[60,179,113],
+limegreen:					[50,205,50],
+darkslategray:				[47,79,79],//alias: darkslategrey
+seagreen:						[46,139,87],
+forestgreen:					[34,139,34],
+lightseagreen:				[32,178,170],
+dodgerblue:					[30,144,255],
+midnightblue:				[25,25,112],
+aqua:								[0,255,255],//basic7, alias: cyan
+springgreen:					[0,255,127],
+lime:								[0,255,0],//basic6
+mediumspringgreen:	[0,250,154],
+darkturquoise:				[0,206,209],
+deepskyblue:				[0,191,255],
+darkcyan:						[0,139,139],
+teal:								[0,128,128],//basic5
+green:							[0,128,0],//basic4
+darkgreen:					[0,100,0],
+blue:								[0,0,255],//basic3
+mediumblue:				[0,0,205],
+navy:								[0,0,128],//basic2
+darkblue:						[0,0,139],
+black:								[0,0,0],//basic1
+transparent:					[0,0,0,0]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
